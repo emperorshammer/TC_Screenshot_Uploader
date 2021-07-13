74bbfe1184a929a99a088cc743423eda4a281c11
Stuart Macintosh <SmTaUcM@Yahoo.co.uk>
2021-07-13 18:26:09 +0100

    Added support for all games and PvP/PvE

---
+++ TC Screenshot Uploader.bat
@@ -0,0 +1,1 @@
+python "TC Screenshot Uploader.py"
--- TC Screenshot Uploader.py
+++ TC Screenshot Uploader.py
@@ -1,12 +1,12 @@
 '''#-------------------------------------------------------------------------------------------------------------------------------------------------#
 # Name:        TC Screenshot Uploader.py
 # Purpose:     A program used to automatically upload game screenshots to the EHTC database.
-# Version:     v1.0.2
+# Version:     v1.0.3
 # Author:      S. Macintosh AKA SkyShadow
 #
-# Created:     25/05/2021
-# Copyright:   None
-# Licence:     None
+# Created:     13/07/2021
+# Copyright:   Open Source
+# Licence:     Open Source
 #-------------------------------------------------------------------------------------------------------------------------------------------------#'''
 
 
@@ -30,11 +30,13 @@ import configparser
 def initialSetup():
     '''Function that sets up this application for use.'''
 
-    # Create the required folder structure.
-    try:
-        os.mkdir(os.getcwd() + "\\Submitted")
-    except FileExistsError:
-        pass # Folder already exists.
+    # Create the required folder structure in each game direcory. e.g. Submitted folders.
+    for option in config.options("folders"):
+        folder = config.get("folders", option)
+        try:
+            os.mkdir(folder + "\\Submitted")
+        except FileExistsError:
+            pass # Folder already exists.
 #----------------------------------------------------------------------------------------------------------------------------------------------------#
 
 
@@ -69,52 +71,284 @@ def windowEnumerationHandler(hwnd, top_windows):
 #----------------------------------------------------------------------------------------------------------------------------------------------------#
 
 
+def askForGame():
+    '''Function asks the user for which game the user is playing.'''
+
+    # XvT2 = XvT, XWA2 = XWA, SC2 = Star Conflict, BF32 = EA-Battlefront 1, EABFII = EA-Battlefront 2, SWS2 = SW Squadrons
+    games = ["xvt", "xwa", "sc", "bf1", "bf2", "sws"]
+
+    #      User Input : [ "PvE Name" , "PvP Name" , "Game Name"]
+    gamesDir = {"xvt" : ["XvT2", "XvT", "X-Wing Vs. Tie Fighter"],
+                "xwa" : ["XWA2", "XWA", "X-Wing Alliance"],
+                "sc" :  ["SC2", "SC", "Star Conflict"],
+                "bf1" : ["BF32", "BF3", "EA Star Wars Battlefront"],
+                "bf2" : ["EABFII", "BF2", "EA Star Wars Battlefront II"],
+                "sws" : ["SWS2", "SWS", "Star Wars Squadrons"]}
+    options = """\n\nxvt = X-Wing Vs. Tie Fighter
+xwa = W-Wing Alliance
+sc = Star Conflict
+bf1 = EA Battlefront 1
+bf2 = EA Battlefront 2
+sws = Star Wars Squadrons"""
+    print(options)
+    gameStr = input("\nWhat game are you playing? ").lower()
+
+    while gameStr not in games:
+        os.system("cls")
+        print(header)
+        print(options)
+        gameStr = input("\nSorry that option isn't available...\n\nWhat game are you playing? ").lower()
+
+    # Get the game mode to be played.
+    mode, modeTitle = askForMode()
+    if mode == "pve":
+        modeNum = 0
+    elif mode == "pvp":
+        modeNum = 1
+
+    return gamesDir[gameStr][modeNum], gamesDir[gameStr][2], mode, modeTitle
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
+def askForMode():
+    '''Function that asks the user for which game mode the user is playing.'''
+
+    # XvT2 = XvT, XWA2 = XWA, SC2 = Star Conflict, BF32 = EA-Battlefront 1, EABFII = EA-Battlefront 2, SWS2 = SW Squadrons
+    modes = ["pve", "pvp"]
+    modeStr = input("\nAre you playing 'PvE' or 'PvP'? ").lower()
+
+    while modeStr not in modes:
+        os.system("cls")
+        print(header)
+        modeStr = input("\nSorry that option isn't available...\n\nAre you playing 'PvE' or 'PvP'? ").lower()
+
+    if modeStr == "pve":
+        gameModeTitle  = " PvE / COOP"
+    elif modeStr == "pvp":
+        gameModeTitle = " PvP"
+
+    return modeStr, gameModeTitle
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+def askForPlayerCount():
+    '''Function that asks the user for how many players are playing.'''
+
+    if mode == "pve":
+        max = 5
+    elif mode == "pvp":
+        max = 10
+
+    count = 0
+    while count <= 0 or count > max or not str(count).isdigit():
+        count = input("How many players are you submitting for? (1 to %s) "%str(max))
+        try:
+            count = int(count)
+        except ValueError:
+            print(count + " is not a valid entry...")
+            count = 0
+
+    return count
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+def askYesNo(text):
+    '''Function that asks the user for a yes no question and returns a boolean response.'''
+
+    response = input(text).lower()
+    while response != "y" and response != "n" and response != "yes" and response != "no":
+        response = input("\n\n%s is not a valid answer..."%response + text).lower()
+
+    if response == "y" or response == "yes":
+        return True
+    elif response == "n" or response == "no":
+        return False
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+def askForWinLose(name):
+    '''Function that asks the user if a player won or loast a match.'''
+
+    response = input("Did %s win or lose the last PvP match? (w/l)"%name).lower()
+    while response != "w" and response != "l" and response != "win" and response != "lose":
+        response = input("\n\n%s is not a valid answer..."%response).lower()
+
+    if response == "w" or response == "win":
+        return "w"
+    elif response == "l" or response == "lose":
+        return "l"
+#----------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
 #----------------------------------------------------------------------------------------------------------------------------------------------------#
 #                                                                     Main Program.                                                                  #
 #----------------------------------------------------------------------------------------------------------------------------------------------------#
-initialSetup()
 config = loadSettings()
+initialSetup()
+
+# Great the user and ask for game parameters.
+header = """\n\n\t################################################################
+\t#      Welcome to SkyShadow's Auto Screenshot Submitter.       #
+\t################################################################"""
+os.system("cls")
+print(header)
+
+# Get the game to be played.
+game, gameName, mode, modeTitle = askForGame()
+gameHeader = "\n\tPlaying " + gameName
+
+# Set the folder to monitor for new screenshots.
+if game == "XvT2" or game == "XvT":
+    screenshotFolder == config.get("folders", "screenshot_folder_xvt")
+elif game == "XWA2" or game == "XWA":
+    screenshotFolder = config.get("folders", "screenshot_folder_xwa")
+elif game == "SC2" or game == "SC":
+    screenshotFolder = config.get("folders", "screenshot_folder_sc")
+elif game == "BF32" or game == "BF3":
+    screenshotFolder = config.get("folders", "screenshot_folder_eabf1")
+elif game == "EABFII" or game == "BF2":
+    screenshotFolder = config.get("folders", "screenshot_folder_eabf2")
+elif game == "SWS2" or game == "SWS":
+    screenshotFolder = config.get("folders", "screenshot_folder_sws")
+
+# Get the game mode to be played.
+os.system("cls")
+print(header)
+print(gameHeader)
+
+# Set the url for uploading screenshots based on game mode selected.
+if mode == "pve":
+    url = config.get("urls", "tc_submit_coop_url")
+elif mode == "pvp":
+    url = config.get("urls", "tc_submit_pvp_url")
+
+gameHeader += modeTitle
+header = """\n\n\t#####################################################
+\t#       SkyShadow's Auto Screenshot Submitter.      #
+\t#####################################################"""
 
 # Main program monitor loop.
+playerPins = [[config.get("user_settings", "pin"), config.get("user_settings", "callsign")]]
+players = 0
+samePlayers = False
+runOnce = False
+
 while True:
-    # Search for screenshots.
-    filenames = glob.glob("*")
+
+    winnerSlot = 1
+    loserSlot = 1
 
     # Intro message.
     os.system("cls")
-    print("\n\n------SkyShadow's Auto Screenshot Submitter.------")
+    print(header)
+    print(gameHeader)
     print("\n\nMonitoring for screenshots...")
+
+    # Search for screenshots.
+    filenames = glob.glob(screenshotFolder + r"\*")
 
     # Auto-submit any found screenshots.
     if filenames != []:
-
-        # Log in to the administration page of the TC website.
-        browser = mechanize.Browser()
-        browser.open(config.get("settings", "tc_admin_url"))
-        browser.form = list(browser.forms())[0]
-        browser["pin"] = config.get("settings", "pin")
-        browser["pass"] = config.get("settings", "password")
-        browser.submit()
-
         for filename in filenames:
             if ".png" in filename or ".jpg" in filename or ".jpeg" in filename:
-                print("\n\nDetected: " + filename)
+                print("Detected: " + filename)
+
+                winners = []
+                losers = []
+
+                # Log in to the administration page of the TC website.
+                browser = mechanize.Browser()
+                browser.open(config.get("urls", "tc_admin_url"))
+                browser.form = list(browser.forms())[0]
+                browser["pin"] = config.get("user_settings", "pin")
+                browser["pass"] = config.get("user_settings", "password")
+                browser.submit()
 
                 # Open and complete the match submission form.
-                browser.open(config.get("settings", "tc_submit_coop_url"))
+                browser.open(url)
                 browser.form = list(browser.forms())[0] # Get the first from from the webpage.
-                browser["game"] = [config.get("settings", "game")]
-                browser["name1w"] = [config.get("settings", "pin")]
-                bringAppToFront()
-                browser["kill1w"] = input("Please enter your number of kills and press 'enter': ")
+                browser["game"] = [game]
+
+                # Add player information to the form.
+                bringAppToFront() # Bring this app to the front and ask the user for the game report details.
+
+                if runOnce:
+                    samePlayers = askYesNo("\n\nUse the same player details from previous game? (y/n): ")
+
+                if not samePlayers:
+                    playerPins = [[config.get("user_settings", "pin"), config.get("user_settings", "callsign")]] # Clear out the old pin data.
+                    players = 0
+
+                if players == 0:
+                    players = askForPlayerCount()
+
+                for player in range(players):
+                    if samePlayers:
+                        pin = playerPins[player][0]
+                        name = playerPins[player][1]
+
+                    else:
+                        if player != 0:
+                            validPin = False
+                            while not validPin:
+                                pin = input("\nPlease enter player %s's TC PIN and press 'enter': "%str(player + 1))
+
+                                try:
+                                    browser["name1w"] = [pin]
+                                    validPin = True
+                                except Exception:
+                                    print("ERROR! %s is not a valid PIN in the TC Database!..."%pin)
+
+                            name = input("Please enter player %s's NAME and press 'enter': "%str(player + 1))
+                            playerPins.append([pin, name])
+
+                        else: # Use the script owners details from settings.ini
+                            pin = playerPins[0][0]
+                            name = playerPins[0][1]
+
+                    if mode == "pvp":
+                        result = askForWinLose(name)
+                    else:
+                        result = "w"
+
+                    kills = input("Please enter %s's number of kills: "%name)
+
+                    if result == "w":
+                        winners.append([pin, name, int(kills)])
+                    elif result == "l":
+                        losers.append([pin, name, int(kills)])
+
+                # Sort and apply the results based on highest score.
+                winners.sort(key=lambda x: (-x[2]))
+                losers.sort(key=lambda x: (-x[2]))
+
+                winnerSlot = 1
+                for player in winners:
+                    result = "w"
+                    slot = winnerSlot
+                    winnerSlot += 1
+                    pin = player[0]
+                    kills = player[2]
+                    browser["name%s%s"%(slot, result)] = [pin]
+                    browser["kill%s%s"%(slot, result)] = str(kills)
+
+                loserSlot = 1
+                for player in losers:
+                    result = "l"
+                    slot = loserSlot
+                    loserSlot += 1
+                    pin = player[0]
+                    kills = player[2]
+                    browser["name%s%s"%(slot, result)] = [pin]
+                    browser["kill%s%s"%(slot, result)] = str(kills)
 
                 # Add the screenshot data and submit the form.
                 screenshot = open(filename, 'rb')
                 browser.form.add_file(screenshot, 'image/png', filename, 'ss1')
                 browser.submit()
                 screenshot.close()
-                shutil.move(filename, "Submitted/" + filename)
+                shutil.move(filename, screenshotFolder + "\\Submitted\\" + filename.split("\\")[-1])
                 print("Upload Complete - " + filename)
+                runOnce = True
                 sleep(2)
 
     sleep(0.5)
